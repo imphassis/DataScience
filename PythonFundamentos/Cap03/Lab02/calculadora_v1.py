f44308aef55ad631f863e4ee3bf3d11604418099
PH Assis <pieit@live.com>
2021-08-10 21:49:32 -0300

    Added calculator function

--- PythonFundamentos/Cap03/Lab02/calculadora_v1.py
+++ PythonFundamentos/Cap03/Lab02/calculadora_v1.py
@@ -5,5 +5,14 @@
 # Assista o vídeo com a execução do programa!
 
 print("\n******************* Python Calculator *******************")
+print("""1 - Soma
+2 - Subtração
+3 - Multiplicação
+4 - Divisão""")
+userChoice = input('Digite o número da operação desejada: ')
+firstNumber = input('Digite o primeiro número: ')
+secondNumber = input('Digite o segundo número: ')
 
-
+oper = {1: lambda x, y: x+y, 2: lambda x, y: x-y, 3: lambda x,y :x*y, 4: lambda x,y :x/y}
+result = oper[int(userChoice)](int(firstNumber), int(secondNumber))
+print(f' O resultado da sua operação é {result}')
